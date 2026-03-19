2da5274834609205ea7b59debe128a382d6d84f9
Matthew Glasenapp <mglasena@ucsc.edu>
2026-03-19 10:48:51 -0700

    update

--- hla_resolve/investigate_haploblocks_methods.py
+++ hla_resolve/investigate_haploblocks_methods.py
@@ -1,9 +1,7 @@
 # This software is Copyright ©2026. The Regents of the University of California
 # ("Regents"). All Rights Reserved.
 #
-# Licensed under the UC Santa Cruz Noncommercial License (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is included in this repository as LICENSE.txt.
+# See LICENSE.txt for license details.
 
 import os
 import csv
