105b00c9454b641a0cec0a661ab074ecfd33eb3e
Matthew Glasenapp <mglasena@ucsc.edu>
2026-03-19 10:37:47 -0700

    update

--- hla_resolve/reconstruct_fasta_methods.py
+++ hla_resolve/reconstruct_fasta_methods.py
@@ -1,3 +1,10 @@
+# This software is Copyright ©2026. The Regents of the University of California
+# ("Regents"). All Rights Reserved.
+#
+# Licensed under the UC Santa Cruz Noncommercial License (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is included in this repository as LICENSE.txt.
+
 import os
 import subprocess
 import pysam
--- supplementary_scripts/sort_cds.py
+++ supplementary_scripts/sort_cds.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+# This software is Copyright ©2026. The Regents of the University of California
+# ("Regents"). All Rights Reserved.
+#
+# Licensed under the UC Santa Cruz Noncommercial License (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is included in this repository as LICENSE.txt.
+
 import os
 import re
 
