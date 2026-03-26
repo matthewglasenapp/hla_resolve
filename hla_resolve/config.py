083fd4dc09577762a85ce72b88975ca27bbf3ad9
Matthew Glasenapp <mglasena@ucsc.edu>
2026-03-26 14:01:43 -0700

    Add ARS depth check (10X mean, >90% bases at 10X))

--- hla_resolve/config.py
+++ hla_resolve/config.py
@@ -272,9 +272,9 @@ prop_20x_thresh = 0.0
 prop_30x_thresh = 0.0
 # ARS (antigen recognition site) thresholds
 # Mean ARS depth
-ars_depth_thresh = 0
+ars_depth_thresh = 10
 # Proportion of ARS bases with depth >= 10x
-ars_prop_10x_thresh = 0.0
+ars_prop_10x_thresh = 0.9
 # Extended MHC coordinates
 mhc_start = 29555628
 mhc_stop = 33409896
